9b0163ef6e0a49be3ffa40f78d6f04317d81e062
jalvaradoc <jalvaradoc@unal.edu.co>
2026-04-19 13:46:01 -0500

    Contenerizado con Docker

--- Monolith/app/connections/postgresql_connection.py
+++ Monolith/app/connections/postgresql_connection.py
@@ -1,15 +1,16 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Me dio pereza poner variables de entorno xd
-# Descomenten la linea de aca abjo y rellen la informacion jaja perdon
-#DATABASE_URL = "postgresql://rellenar1@rellnar2"
+load_dotenv()
 
-# 1.                     postgres:admin
-# 2.                                    localhost:5432/arqui
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
