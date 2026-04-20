6a26d8e856a2136ea8af83251a14685a4929150e
itsLisis <feduque@unal.edu.co>
2026-04-20 12:35:21 -0500

    Prototype 1 complete: Uploading final features.
    Some fine-tuning is needed.

--- Monolith/app/main.py
+++ Monolith/app/main.py
@@ -4,11 +4,13 @@ from fastapi.templating import Jinja2Templates
 from app.connections.postgresql_connection import Base, engine
 from app.models.user import User
 from app.routers.auth import router as auth_router
+from app.routers.match import router as match_router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(auth_router)
+app.include_router(match_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.on_event("startup")
