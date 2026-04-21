5d42f35da4ac338c73c477fdec0980290fb20ae6
jalvaradoc <jalvaradoc@unal.edu.co>
2026-04-20 19:04:06 -0500

    Add Firebase credentials configuration via .env

--- Monolith/app/connections/firebase_connection.py
+++ Monolith/app/connections/firebase_connection.py
@@ -1,9 +1,14 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 
-# Esto es la parte hardcodeada, no supe como usar variables de entorno ya que pide un json :(
-cred = credentials.Certificate("ruta al .json de las credenciales")
+load_dotenv()
+
+cred_path = os.getenv("FIREBASE_CREDENTIALS")
+cred = credentials.Certificate(cred_path)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
